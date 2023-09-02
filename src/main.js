5b65df32c8be51aeb2714c1af936601967d06ceb
danielNKURUNZIZA <dnkurunziza979@gmail.com>
2023-09-02 23:35:26 +0200

    final committ

--- src/main.js
+++ src/main.js
@@ -1,3 +1,37 @@
+function formValidation()
+{
+    var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+    var name=document.form.Name.value;
+    var email=document.form.Email.value;
+    var subject=document.form.Subject.value;
+    var message=document.form.Message.value;
+
+
+
+    if (email == "" || !regEmail.test(email)) {
+        alert("Please enter a valid e-mail address.");
+        email.focus();
+        return false;
+    }
+    if (message.length<10 || message.length >500) {
+        window.alert("Message should be atleast 10 charcters and maximum of 500 character long");
+        address.focus();
+        return false;
+    }
+    if (subject.length<10 || subject.length >500) {
+        window.alert("subject  should be atleast 10 charcters and maximum of 50 character long");
+        address.focus();
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
 const swiper=new swiper('.swiper',{
     scrollbar:{
         el:'.swiper-scrollbar',
