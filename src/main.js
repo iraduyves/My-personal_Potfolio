4697b4ccac1a06a50c21bc73fa37891e69e39d49
danielNKURUNZIZA <dnkurunziza979@gmail.com>
2023-09-07 22:37:47 +0200

    final commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-function formValidation()
-{
-    var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-    var name=document.form.Name.value;
-    var email=document.form.Email.value;
-    var subject=document.form.Subject.value;
-    var message=document.form.Message.value;
+function formValidation() {
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+    var name = document.form.Name.value;
+    var email = document.form.Email.value;
+    var subject = document.form.Subject.value;
+    var message = document.form.Message.value;
 
 
 
@@ -13,12 +12,12 @@ function formValidation()
         email.focus();
         return false;
     }
-    if (message.length<10 || message.length >500) {
+    if (message.length < 10 || message.length > 500) {
         window.alert("Message should be atleast 10 charcters and maximum of 500 character long");
         address.focus();
         return false;
     }
-    if (subject.length<10 || subject.length >500) {
+    if (subject.length < 10 || subject.length > 500) {
         window.alert("subject  should be atleast 10 charcters and maximum of 50 character long");
         address.focus();
         return false;
@@ -32,49 +31,57 @@ function formValidation()
 
 
 
-const swiper=new swiper('.swiper',{
-    scrollbar:{
-        el:'.swiper-scrollbar',
-        draggable:true,
-    },
-});
+// const swiper = new swiper('.swiper', {
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//         draggable: true,
+//     },
+// });
 
-const menuBtn=document.querySelector('#menu-btn');
-const closeBtn=document.querySelector('#close-btn');
+// const menuBtn=document.querySelector('#menu-btn');
+// const closeBtn=document.querySelector('#close-btn');
+const nav=document.querySelector('nav .container');
 const menu=document.querySelector('nav .container ul');
 
-menuBtn.addEventListener('click',() => {
-    menu.style.display='block';
-    menuBtn.style.display='none';
-    closeBtn.style.display='inline-block';
-})
+// menuBtn.addEventListener('click',() => {
+//     menu.style.display='block';
+//     menuBtn.style.display='none';
+//     closeBtn.style.display='inline-block';
+// })
 
-closeBtn.addEventListener('click',() => {
-    menu.style.display='none';
-    menuBtn.style.display='inline-block';
-    closeBtn.style.display='none';
-})
+// closeBtn.addEventListener('click',() => {
+//     menu.style.display='none';
+//     menuBtn.style.display='inline-block';
+//     closeBtn.style.display='none';
+// })
 
-const navItems=menu.querySelectorAll('li');
-const changeActiveItem = ()=>{
-    navItems.forEach(item =>{
-        const link= item.querySelector('a');
-        link.classList.remove('active');
+console.log(menu)
+const navInput = nav.querySelector('input#nav-input');
+const navItems = menu.querySelectorAll('li');
+navItems.forEach(item => {
+    item.addEventListener('click', e => {
+        console.log(navInput)
+        navInput.checked = false
     })
-}
+})
 
-navItems.forEach(item =>{
-    const link=item.querySelector('a');
-    link.addEventListener('click', ()=>{
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        menu.querySelectorAll('nav .container ul .active').forEach(li => {
+            li.classList.remove('active')
+
+        })
+
         link.classList.add('active');
     })
 })
 
 
-const readMoreBtn= document.querySelector('.read-more');
-const readMoreContent=document.querySelector('read-more-content');
+const readMoreBtn = document.querySelector('.read-more');
+const readMoreContent = document.querySelector('read-more-content');
 
-readMoreBtn.addEventListener('click',()=>{
+readMoreBtn.addEventListener('click', () => {
     readMoreContent.classList.toggle('show-content');
 })
 
@@ -84,10 +91,10 @@ readMoreBtn.addEventListener('click',()=>{
 
 
 
-const skillItems=document.querySelectorAll('section.skills .skill');
+const skillItems = document.querySelectorAll('section.skills .skill');
 
-skillItems.forEach(skill=>{
-    skill.querySelector('.head').addEventListener('click',()=>{
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
         skill.querySelector('.items'.classList.toggle('show-items'))
     })
 })
